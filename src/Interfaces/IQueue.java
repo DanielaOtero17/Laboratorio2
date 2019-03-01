c688304270852bf13f2890f0a9f983eb4535ca39
1144209785 <1144209785@SC208E08.academico.icesi.edu.co>
2019-03-01 13:04:12 -0500

    // iQUEUE AND QUEUE IN CONTRUCTION

--- src/Interfaces/IQueue.java
+++ src/Interfaces/IQueue.java
@@ -2,4 +2,11 @@ package Interfaces;
 
 public interface IQueue<T> {
 	
+	public void insert(T objet ) ;
+	public T firts();
+	public T lastElement ();
+	public T outElement();
+	public T getNext();
+	public int getSize();
+	public T setNext(T element);
 }
--- src/Model/Queue.java
+++ src/Model/Queue.java
@@ -1,5 +1,90 @@
 package Model;
 
-public class Queue {
+import java.util.LinkedList;
+
+import Interfaces.IQueue;
+
+public class Queue <T> implements IQueue <T> {
+	
+	private T firts;
+	private T lastElement;
+	
+	
+	
+	public Queue () {
+		
+		firts = null;
+		lastElement = null;
+		
+	}
+
+
+	@Override
+	public void insert(T object) {
+
+	   if (this.getSize() == 0) {
+			
+		    firts = object;
+			lastElement = object;
+
+		} else if (this.getSize()!= 0) {
+            
+			  if(this.getNext()==null) {
+				  lastElement=object;
+			    }
+		}else { 
+			while(this.getNext()!=null) {
+				this.getNext();
+			}
+			lastElement = object;
+		}
+
+	}
+
+	@Override
+	public T firts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public T lastElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public T outElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public T getNext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public T setNext(T element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+	
+	
 
 }
