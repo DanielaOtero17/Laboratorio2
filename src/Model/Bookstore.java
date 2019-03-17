3adba2950104efbbdb4eff529c493996695b27ae
DanielaOtero17 <jessot@hotmail.com>
2019-03-16 22:12:37 -0500

    comments has been added.

--- src/Model/Bookstore.java
+++ src/Model/Bookstore.java
@@ -22,25 +22,29 @@ public class Bookstore {
 		booksout = 0;
 	}
 		
+	// Arreglo que almacena los cajeros existentes.
 	public ArrayList<Shelving<String,Integer>> getShelves() {
 		return shelves;
 	}
+	
+	// metodo set de los cajeros.
 	public void setShelves(ArrayList<Shelving<String,Integer>> shelves) {
 		this.shelves = shelves;
 	}
 	
-	public void addClientPay(Customer c){
-		payList.enqueue(c);
-	}
-	
+	// set de cantidad de libros que serán vendidos.
 	public void setBooksout(int x) {
 		
 		booksout = x;
 	}
+	
+	// retorna la cantidad de libros que serán vendidos.
 	public int getBooksout() {
 		return booksout;
 	}
 
+	// Organiza la lista de clientes teniendo en cuenta su tiempo en la librería
+	// agregando a los clientes en la cola de pago ( PayList)
 	public void organizeListPay(){
 		
 		Customer[] aux = new Customer[employees.size()];
@@ -58,6 +62,9 @@ public class Bookstore {
 		}		
 	}
 	
+	// Este método es el encargado de retornar el listado de datos
+	// de los clientes al momento de salir de la librería, teniendo en cuenta su orden en la cola
+	// de pago y la cantidad de libros que van a comprar.
 	public String processPaying(){
 		
 		String data = "";
@@ -115,6 +122,8 @@ public class Bookstore {
 				return data;
 	}
 	
+	// este método es auxiliar, para verificar si hay algun elemento de un arreglo booleano 
+	// con valor false.
 	public boolean exist(boolean[] bool){
 		
 		for(int i=0; i<bool.length;i++){
@@ -127,22 +136,8 @@ public class Bookstore {
 		return false;
 	}
 	
-//	public void addCustomerATM() {
-//		
-//		organizeListPay();
-//		
-//		for(int i=0; i< atms.size(); i++) {
-//			
-//			ATM aux = this.isNotBusy();
-//			int a = isTheSame(aux);
-//			
-//			if(aux!=null) {
-//				
-//				atms.get(a).addBooksPay(payList.Dequeue());				
-//			}
-//		}
-//	}
-//	
+	// Este método verifica si un cajero que ingresa por parámetro, es
+	// igual a uno de los cajeros pertenecientes a la librería.
 	public int isTheSame(ATM atm) {
 		
 		for(int i=0; i<atms.size();i++) {
@@ -155,6 +150,8 @@ public class Bookstore {
 		return 0;
 	}
 	
+	// Este método verifica si un cajero de la librería se encuentra desocupado, y lo retorna.
+	// en caso de no encontrar ningún cajero que esté disponible, retorna null.
 	public ATM isNotBusy() {
 		
 		for(int i=0; i<atms.size();i++) {
@@ -168,10 +165,15 @@ public class Bookstore {
 		
 	}
 	
+	// Este método agrega un cajero al arreglo de cajeros de la librería.
 	public void addATM(ATM adding){
 		
 		atms.add(adding);
 	}
+	
+	// Este método crea un nuevo cliente. Le asigna el tiempo de entrada al cliente dependiendo
+	// de el orden en el que haya llegado a la librería.
+	// También agrega el cliente a la lista de clientes de la librería.
 	public void createCustomer(Customer e){
 		
 		e.setTime(employees.size()+1);//incrementa el tiempo del cliente.
@@ -179,10 +181,12 @@ public class Bookstore {
 		
 		}
 		
+	// retorna la cola de pago.
 	public Queue<Customer> getPayList() {
 		return payList;
 	}
-
+	
+	// cambia la cola de pago.
 	public void setPayList(Queue<Customer> payList) {
 		this.payList = payList;
 	}
@@ -208,11 +212,13 @@ public class Bookstore {
 		return null;
 		}
 	
+	// Este método agrega una Estantería genérica a la lista de estanterías de la librería.
 	public void addShelves(Shelving<String, Integer> shelve){
 		
 		shelves.add(shelve);		
 	}
 	
+	// Este método retorna la cantidad total de libros que existen en la librería.
 	public int getTotalBooks(){
 		
 		int sum = 0;
@@ -222,6 +228,9 @@ public class Bookstore {
 		}
 		return sum;
 	}	
+	
+	// Este Método crea un nuevo libro. y lo agraga a la lista de libros de la librería
+	// Lo ubica en su respectiva estantería y peldaño.
 	public void createNewBook(int code,int examples, String shelve,int price) throws IOException {
 		
 		Book new_book = new Book(code,examples,shelve,price);
@@ -229,12 +238,16 @@ public class Bookstore {
 	shelves.get(positionShelve(shelve)).addBook(new_book.getCode(), new_book.getQuantity(),new_book.getPrice());		
 	}
 	
+	
+	// Este método retorna el tamaño de la lista de libros. Es un auxiliar de verificación
+	// Para saber si el tamaño de la lista de libros es la misma cantidad de libros totales.
 	public int getTamanioListBooks(){
 		
 		System.out.println("El tamanio de la lista es " + list.size());
 		return list.size();
 	}
 	
+	// Este método retorna la posición de una estantería, basado en su código.
 	public int positionShelve(String name) throws IOException{
 		
 		int i= 0;
@@ -248,6 +261,8 @@ public class Bookstore {
 		return 0;
 		
 	}
+	
+	// este método verifica si existe una estantería con cierto código ingresado.
 	public boolean existShelve(String name){
 		int i= 0;
 		while(i<shelves.size()){
@@ -260,6 +275,7 @@ public class Bookstore {
 		return false;
 	}
 	
+	// Este método verifica si existe un libro dado su codigo isbn.
 	public boolean existBook(int cod){
 		
 		
@@ -271,6 +287,7 @@ public class Bookstore {
 		return false;
 	}
 	
+	// Este método resta la cantidad de libros.
 	public void restQuantity(Book b){
 		
 		b.setQuantity(b.getQuantity()-1);
